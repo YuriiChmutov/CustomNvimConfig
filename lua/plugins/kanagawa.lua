7627d0c85c13c849e8bdcbfe8e8cc243a8576aa5
Yurii Chmutov <vad4807@gmail.com>
2025-06-19 11:36:33 +0300

    Modify numbers color

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -9,7 +9,7 @@ return {
         local kanagawa_colors = require("kanagawa.colors").setup()
         local palette = kanagawa_colors.palette
 
-        vim.api.nvim_set_hl(0, "LineNr", { fg = palette.sumiInk3, bg = "NONE" })
+        vim.api.nvim_set_hl(0, "LineNr", { fg = palette.winterGreen, bg = "NONE" })
         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = palette.dragonYellow, bold = true, bg = "NONE" })
     end
 }
