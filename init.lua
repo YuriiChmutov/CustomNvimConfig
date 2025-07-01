b735e291d8b6fda887d88fe7c21d96ead03d94e4
Yurii Chmutov <vad4807@gmail.com>
2025-07-01 08:49:06 +0300

    Show specific theme depend on command

--- init.lua
+++ init.lua
@@ -22,11 +22,14 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 vim.o.number = true
+-- show warnings/errors descriptions in 1 second (by defaut it takes 4 seconds)
+vim.o.updatetime = 1000
 -- vim.o.relativenumber = true
 
 require("vim-options")
 require("lazy").setup("plugins")
 require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/lua/snippets" })
+require("luasnip.loaders.from_vscode").lazy_load()
 require("keymaps")
 
 require("neo-tree").setup({
@@ -36,6 +39,38 @@ require("neo-tree").setup({
     },
   },
 })
+
+-- START: To load specific theme depend on command --
+
+local args = vim.fn.argv()
+local theme_found = false
+
+for _, arg in ipairs(args) do
+  if arg:match("^Theme:") then
+    theme_found = true
+    local theme = arg:match("^Theme:(.*)")
+    if theme == "onedark" then
+      require("onedark").setup { style = "darker" }
+      require("onedark").load()
+    elseif theme == "kanagawa" then
+      vim.cmd.colorscheme("kanagawa-dragon")
+    end
+  end
+end
+
+if not theme_found then
+  vim.cmd.colorscheme("kanagawa-dragon")
+end
+
+if vim.g.colors_name == "kanagawa-dragon" or not theme_found then
+  local kanagawa_colors = require("kanagawa.colors").setup()
+  local palette = kanagawa_colors.palette
+
+  vim.api.nvim_set_hl(0, "LineNr", { fg = palette.winterGreen, bg = "NONE" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = palette.dragonYellow, bold = true, bg = "NONE" })
+end
+
+-- END --
 
 -- vim.cmd [[
 --   autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focus = false })
--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -1,15 +1,44 @@
+-- return 
+-- {
+--     "rebelot/kanagawa.nvim",
+--     name = "kanagawa",
+--     priority = 1000,
+--     lazy = false,
+--     config = function()
+--         vim.cmd[[colorscheme kanagawa-dragon]]
+
+--         local kanagawa_colors = require("kanagawa.colors").setup()
+--         local palette = kanagawa_colors.palette
+
+--         vim.api.nvim_set_hl(0, "LineNr", { fg = palette.winterGreen, bg = "NONE" })
+--         vim.api.nvim_set_hl(0, "CursorLineNr", { fg = palette.dragonYellow, bold = true, bg = "NONE" })
+--     end
+-- }
+
+-- {
+--   "navarasu/onedark.nvim",
+--   priority = 1000, -- make sure to load this before all the other start plugins
+--   config = function()
+--     require('onedark').setup {
+--       style = 'darker'
+--     }
+--     -- Enable theme
+--     require('onedark').load()
+--   end
+-- }
+
 return {
-    "rebelot/kanagawa.nvim",
-    name = "kanagawa",
-    priority = 1000,
-    lazy = false,
-    config = function()
-        vim.cmd[[colorscheme kanagawa-dragon]]
+    {
+        "rebelot/kanagawa.nvim",
+        name = "kanagawa",
+        priority = 1000,
+        lazy = false,
+    },
 
-        local kanagawa_colors = require("kanagawa.colors").setup()
-        local palette = kanagawa_colors.palette
-
-        vim.api.nvim_set_hl(0, "LineNr", { fg = palette.winterGreen, bg = "NONE" })
-        vim.api.nvim_set_hl(0, "CursorLineNr", { fg = palette.dragonYellow, bold = true, bg = "NONE" })
-    end
+    {
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        lazy = false,
+    }
 }
+
