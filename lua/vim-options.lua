d7fe1b69c84f506f4a27cb5a88d53b05eed10e47
Yurii Chmutov <vad4807@gmail.com>
2025-07-01 08:49:51 +0300

    Indents for specific extensions

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,3 +3,13 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
+
+-- Specific indents for files with specific extensions
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "xml", "javascriptreact", "typescriptreact", "tsx", "jsx", "vue", "svelte" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
