beeda729686847ee6be81297124a63f42173e0f0
Yurii Chmutov <vad4807@gmail.com>
2025-07-01 08:52:26 +0300

    Enhance LSP diagnostics configuration and redefine error symbol

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -16,6 +16,25 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function()
+
+            vim.o.signcolumn = "yes"
+
+            -- Setting up the display of diagnostics (includes displaying the text which shows errors/warnings)
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+                severity_sort = true,
+            })
+
+            -- Redefining Error Symbols
+            local signs = { Error = "", Warn = "", Hint = "", Info = "" }
+            for type, icon in pairs(signs) do
+                local hl = "DiagnosticSign" .. type
+                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+            end
+
             local lspconfig = require("lspconfig")
             local capabilities = require('cmp_nvim_lsp').default_capabilities()
             -- Lua
@@ -61,6 +80,10 @@ return {
                 capabilities = capabilities,
             })
 
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities,
+            })
+
             vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
             vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
             vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
@@ -69,7 +92,7 @@ return {
             vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { desc = "Rename symbol" })
 
             vim.cmd [[
-                autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focus = false })
+                autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focus = false, border = "rounded" })
             ]]
         end
     }
